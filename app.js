71a58522ecf258a495f1b70f9c255f4fe24e2a4d
Ope <succesamust@gmail.com>
2025-11-01 01:25:02 +0100

    minor fixes

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 
 const app = express()
+dotenv.config();
 
 app.use(cors({origin: "*"}))
-app.use(bodyParser.json())
-app.use(bodyParser({urlencoded: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const PORT = process.env.PORT || 3050;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
